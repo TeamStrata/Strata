542289210761b126b80612b685cd8cc38b034b11
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-03-15 22:23:53 -0400

    feat: write db function to get user by user name

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -7,9 +7,35 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type User struct {
+	Name     string
+	Password string
+}
+
 type DbManager struct {
 	conStr     string
 	Connection *pgxpool.Pool
+	context    context.Context
+}
+
+func NewDbMananger(connectionString string, ctx context.Context) (*DbManager, error) {
+	if len(connectionString) == 0 {
+		msg := "error creating new DbManager: empty connection string"
+		return nil, errors.New(msg)
+	}
+
+	dbManager := DbManager{
+		conStr:     connectionString,
+		Connection: nil,
+		context:    ctx,
+	}
+
+	err := dbManager.ConnectToDatabase()
+	if err != nil {
+		return nil, err
+	}
+
+	return &dbManager, nil
 }
 
 // Set connection string
@@ -28,4 +54,20 @@ func (d *DbManager) ConnectToDatabase() error {
 	d.Connection, err = pgxpool.New(context.Background(), d.conStr)
 
 	return err
+}
+
+func (d *DbManager) GetUserByUserName(name string) (User, error) {
+	user := User{}
+	var err error
+	if err = d.Connection.Ping(context.Background()); err != nil {
+		return User{}, nil
+	}
+
+	query := "SELECT user_name, password_hash FROM users WHERE user_name = $1"
+	err = d.Connection.QueryRow(d.context, query, name).Scan(&user.Name, &user.Password)
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
 }
