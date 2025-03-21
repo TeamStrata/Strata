95a89faefdfc72c2206bc939c68e3b6644213c90
Alistair Campbell <ajcampbell@conestogac.on.ca>
2025-03-21 19:23:51 -0400

    Added placeholder for full admin page

---
+++ ui/src/views/Admin.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+
+
+</script>
+
+<template>
+    <div>
+        <!-- sidebar -->
+        <div>
+            <RouterLink to="/admin/users">Users</RouterLink>
+        </div>
+        <!-- focus area -->
+        <RouterView></RouterView>
+    </div>
+</template>
