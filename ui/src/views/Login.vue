c373825e242140d5daf9675a333931956cdd4913
Alistair Campbell <ajcampbell@conestogac.on.ca>
2025-03-21 20:04:41 -0400

    Slapped some preliminary styling on the login page

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -13,12 +13,18 @@ async function login() {
 </script>
 
 <template>
-    <div>
-        <label for="username">Username</label>
-        <input id="usernam" v-model="user">
-        <label for="password">Password</label>
-        <input id="password" v-model="pass">
-        <button @click="login">Login</button>
-        <p v-if="error">Something went wrong</p>
+    <div class="flex h-screen bg-gray-100">
+        <div class="flex flex-col w-96 m-auto p-8 bg-white shadow-md rounded-lg">
+            <h1 class="text-2xl font-semibold text-gray-800 mb-4">Log In</h1>
+            <label for="username" class="text-gray-600">Username</label>
+            <input id="username" v-model="user" class="mt-1 border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Enter your username">
+            
+            <label for="password" class="mt-4 text-gray-600">Password</label>
+            <input id="password" type="password" v-model="pass" class="mt-1 border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Enter your password">
+
+            <button @click="login" class="mt-6 bg-blue-500 text-white font-semibold py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-200">Login</button>
+            
+            <p v-if="error" class="mt-4 text-red-500">Something went wrong</p>
+        </div>
     </div>
 </template>
