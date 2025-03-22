94089ce8bb11147f0b6cb5d102e24bbe16dc0a2c
Alistair Campbell <ajcampbell@conestogac.on.ca>
2025-03-22 13:45:27 -0400

    Added API integration for admin create user

--- ui/src/components/AdminUsers.vue
+++ ui/src/components/AdminUsers.vue
@@ -9,18 +9,50 @@ let users =
 var newUsername = ref("");
 var newPassword = ref("");
 
+
+function addUser() {
+    // submit user and pass to form
+    fetch('http://localhost:8080/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: newUsername.value,
+            password: newPassword.value
+        }),
+    })
+        .then(response => {
+            if (!response.ok) {
+                error = true;
+                throw new Error('Something went wrong');
+            }
+            let data = response.json();
+            console.log(data)
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
 </script>
 
 <template>
-    <h1>Manage Users</h1>
+    <h1 class="font-bold">Manage Users</h1>
     <!-- new user area -->
-    <div>
-        <label for="newUser">Username:</label>
-        <input id="newUser" v-model="newUsername">
-        <label for="newPass">Password:</label>
-        <input id="newPass" type="password" v-model="newPassword">
-        <button class="border-2 rounded-md p-2 cursor-pointer">Add User</button>
+     add user placeholder
+    <div class="flex flex-col">
+        <div>
+            <label for="newUser">Username:</label>
+            <input id="newUser" v-model="newUsername">
+        </div>
+        <div>
+            <label for="newPass">Password:</label>
+            <input id="newPass" type="password" v-model="newPassword">
+        </div>
+        <button class="bg-blue-100 rounded-sm w-fit px-2 cursor-pointer" @click="addUser">Add User</button>
     </div>
+
+    Users list placeholder:
     <ul>
         <li v-for="u in users">
             {{ u.name }}
@@ -28,3 +60,10 @@ var newPassword = ref("");
     </ul>
 
 </template>
+
+<style scoped>
+input {
+    background-color: rgb(230, 230, 230);
+    margin: 1px;
+}
+</style>
