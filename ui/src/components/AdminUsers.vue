c80f2acd096066c22569d32fd5bc4254b09f5f2c
Alistair Campbell <ajcampbell@conestogac.on.ca>
2025-03-22 09:32:44 -0400

    Added user management page

--- ui/src/components/AdminUsers.vue
+++ ui/src/components/AdminUsers.vue
@@ -1,10 +1,30 @@
 <script setup>
+import { ref } from 'vue';
+// placeholder user data 
+let users =
+    [
+        { name: "Alistair", stuff: "hello" },
+        { name: "Sebastian", stuff: "hello" }
+    ]
+var newUsername = ref("");
+var newPassword = ref("");
 
 </script>
 
 <template>
+    <h1>Manage Users</h1>
+    <!-- new user area -->
     <div>
-        Users Page
+        <label for="newUser">Username:</label>
+        <input id="newUser" v-model="newUsername">
+        <label for="newPass">Password:</label>
+        <input id="newPass" type="password" v-model="newPassword">
+        <button class="border-2 rounded-md p-2 cursor-pointer">Add User</button>
     </div>
+    <ul>
+        <li v-for="u in users">
+            {{ u.name }}
+        </li>
+    </ul>
 
 </template>
