1d3a1f585071cbc1437a1b393e2005babd16d1e0
Alistair Campbell <ajcampbell@conestogac.on.ca>
2025-03-22 13:35:42 -0400

    Tweaked button name

--- ui/src/views/Admin.vue
+++ ui/src/views/Admin.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router';
     <div class="flex flex-row max-w-[1000px] mx-auto h-screen p-20">
         <!-- sidebar -->
         <div class="bg-red-400 w-3xs h-full p-5">
-            <RouterLink to="/admin/users">Users</RouterLink>
+            <RouterLink to="/admin/users">Users Button</RouterLink>
         </div>
         <div class="w-full h-full p-5">
             <!-- focus area -->
