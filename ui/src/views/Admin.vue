32bbf32a6a667cb6bea6513ee0489b6cbe420895
Alistair Campbell <ajcampbell@conestogac.on.ca>
2025-03-22 09:33:21 -0400

    Tweaked spacing on admin page

--- ui/src/views/Admin.vue
+++ ui/src/views/Admin.vue
@@ -7,10 +7,10 @@ import { RouterLink, RouterView } from 'vue-router';
 <template>
     <div class="flex flex-row max-w-[1000px] mx-auto h-screen p-20">
         <!-- sidebar -->
-        <div class="bg-red-400 w-3xs h-full">
+        <div class="bg-red-400 w-3xs h-full p-5">
             <RouterLink to="/admin/users">Users</RouterLink>
         </div>
-        <div class="w-full h-full">
+        <div class="w-full h-full p-5">
             <!-- focus area -->
             <RouterView></RouterView>
         </div>
