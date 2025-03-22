0c11db86024752d871cf20ca75d5e3c2b782d555
Alistair Campbell <ajcampbell@conestogac.on.ca>
2025-03-21 20:52:07 -0400

    Sketched out the home page layout (VERY WIP)

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,9 +1,54 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { RouterLink } from 'vue-router';
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- main separator -->
+  <div class="flex flex-row h-screen">
+    <!-- sidebar -->
+    <div class="w-xs h-full bg-amber-100 flex flex-col">
+      <!-- logo -->
+      <div class="">
+        Logo Placeholder
+      </div>
+      <!-- admin controls -->
+      <div>
+        <RouterLink to="/admin">Admin button</RouterLink>
+      </div>
+      <!-- navigation stuff -->
+      <div class="flex flex-col">
+        Navigation stuff
+        <RouterLink to="/">Link</RouterLink>
+      </div>
+    </div>
+    <!-- right side -->
+    <div class="flex flex-col">
+      <!-- header bar -->
+      <div>
+        <button>Logout</button>
+      </div>
+      <!-- main content area -->
+      <div>
+        THERE SHOULD BE CONTENT HERE BUT WE HAVE NO CONTENT TO PUT HERE YET
+      </div>
+    </div>
+  </div>
 </template>
+
+
+
+<!-- <template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">login</RouterLink>
+        <RouterLink to="/admin">Admin</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <p>Huge content placeholder</p> 
+</template> -->
